afdb19bfd5fc46d9bd75df73f74a0fd6ebf6db9a
Maximilian Beil <maximilian.beil2779@gmail.com>
2017-12-07 12:33:24 +0100

    meida changes

--- src/AppBundle/Controller/Media/MediaController.php
+++ src/AppBundle/Controller/Media/MediaController.php
@@ -17,11 +17,9 @@ class MediaController extends Controller
     public function getMediaAction(Request $request,Connection $connection)
     {
         $result = $connection->fetchAll("
-        select d.id,m.duration,d.title, d.description,m.thumbnail,max(m.season) as season_count from media m
+        select d.id,m.duration,d.title, d.description,max(m.season)as season_count,m.thumbnail  from media m
         join media_description d on d.id = m.description_id
-        join media_category mc on mc.description_id = d.id
-        join category c on mc.category_id = c.id
-        group by m.id
+        group by m.description_id
         ");
 
         return new JsonResponse($result);
