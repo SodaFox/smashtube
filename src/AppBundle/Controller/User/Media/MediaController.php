1ac60115343d528336806d0356fcfab5176ff12a
Maximilian Beil <maximilian.beil2779@gmail.com>
2017-12-11 12:04:17 +0100

    watchlist changes

--- src/AppBundle/Controller/User/Media/MediaController.php
+++ src/AppBundle/Controller/User/Media/MediaController.php
@@ -35,11 +35,10 @@ class MediaController extends Controller
         join media m on m.id = wl.media_id
         join media_description md on md.id = m.description_id
         where user_id = ?",
-        array($user->getId()));
+            array($user->getId()));
 
         return new JsonResponse($result);
     }
-
     /**
      * @Route("/user/media/watchlist")
      * @Method({"POST"})
@@ -62,27 +61,16 @@ class MediaController extends Controller
             return new JsonResponse(false);
         }
     }
-
     /**
      * @Route("/user/media/watchlist")
      * @Method({"DELETE"})
      */
     public function deleteWatchlistAction(Request $request,Connection $connection)
     {
-        $formData = $request->request->all();
+        $episodeId = $request->query->get("episode_id");
 
-        $result = $connection->insert("watch_list",array(
-            "media_id" => $formData["episode_id"],
-            "user_id" => $this->getUser()->getId()
-        ));
-
-        if($result > 0)
-        {
-            return new JsonResponse(true);
-        }
-        else
-        {
-            return new JsonResponse(false);
-        }
+        $result = $connection->executeQuery("delete from watch_list where media_id = ? and user_id = ?",
+            array($episodeId,$this->getUser()->getId()));
+        return new JsonResponse();
     }
 }
