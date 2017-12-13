5596ad9c121febc909949e8dcbd4579c7d4b1385
Selisch <a.selisch@xgx.at>
2017-12-13 08:40:11 +0100

    [UPDATE] updated database structure

--- app/sql/smashtube.sql
+++ app/sql/smashtube.sql
@@ -1,32 +1,77 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 13, 2017 at 07:35 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
 
-CREATE DATABASE IF NOT EXISTS `smashtube` /*!40100 DEFAULT CHARACTER SET utf8 */;
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `smashtube`
+--
+CREATE DATABASE IF NOT EXISTS `smashtube` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `smashtube`;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_contact`
+--
+
+DROP TABLE IF EXISTS `admin_contact`;
 CREATE TABLE IF NOT EXISTS `admin_contact` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(256) NOT NULL,
   `is_contact` tinyint(4) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `genre` varchar(256) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `design`
+--
+
+DROP TABLE IF EXISTS `design`;
 CREATE TABLE IF NOT EXISTS `design` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `description` varchar(500) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media`
+--
+
+DROP TABLE IF EXISTS `media`;
 CREATE TABLE IF NOT EXISTS `media` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(256) DEFAULT NULL,
@@ -39,19 +84,30 @@ CREATE TABLE IF NOT EXISTS `media` (
   `description_id` int(11) NOT NULL,
   `episode_number` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_media_media_ov` (`description_id`),
-  CONSTRAINT `media_media_description` FOREIGN KEY (`description_id`) REFERENCES `media_description` (`id`) ON UPDATE NO ACTION
+  KEY `FK_media_media_ov` (`description_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media_category`
+--
+
+DROP TABLE IF EXISTS `media_category`;
 CREATE TABLE IF NOT EXISTS `media_category` (
   `description_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   PRIMARY KEY (`description_id`,`category_id`),
-  KEY `media_category_categoryId` (`category_id`),
-  CONSTRAINT `media_category_categoryId` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON UPDATE NO ACTION,
-  CONSTRAINT `media_category_mediaId` FOREIGN KEY (`description_id`) REFERENCES `media_description` (`id`) ON UPDATE NO ACTION
+  KEY `media_category_categoryId` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media_description`
+--
+
+DROP TABLE IF EXISTS `media_description`;
 CREATE TABLE IF NOT EXISTS `media_description` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(256) NOT NULL,
@@ -59,12 +115,26 @@ CREATE TABLE IF NOT EXISTS `media_description` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
 CREATE TABLE IF NOT EXISTS `question` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `text` varchar(256) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timestamp`
+--
+
+DROP TABLE IF EXISTS `timestamp`;
 CREATE TABLE IF NOT EXISTS `timestamp` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
@@ -72,40 +142,94 @@ CREATE TABLE IF NOT EXISTS `timestamp` (
   `timestamp` time NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_timestamp_media` (`media_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `FK_timestamp_media` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`),
-  CONSTRAINT `FK_timestamp_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(256) NOT NULL,
   `birthday` date NOT NULL,
-  `is_admin` tinyint(1) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
   `password` varchar(256) NOT NULL,
   `answer` varchar(256) NOT NULL,
   `salt` varchar(50) DEFAULT NULL,
   `roles` varchar(100) DEFAULT NULL,
   `question_id` int(11) NOT NULL,
-  `design_id` int(10) unsigned NOT NULL,
+  `design_id` int(10) UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `question_id` (`question_id`),
-  KEY `design_id` (`design_id`),
-  CONSTRAINT `FK_user_design` FOREIGN KEY (`design_id`) REFERENCES `design` (`id`),
-  CONSTRAINT `question_id` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
+  KEY `design_id` (`design_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `watch_list`
+--
+
+DROP TABLE IF EXISTS `watch_list`;
 CREATE TABLE IF NOT EXISTS `watch_list` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `media_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  KEY `media_id` (`media_id`),
-  CONSTRAINT `FK_Verlauf_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_watch_history_media_description` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`)
+  KEY `media_id` (`media_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admin_contact`
+--
+ALTER TABLE `admin_contact`
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `media`
+--
+ALTER TABLE `media`
+  ADD CONSTRAINT `media_media_description` FOREIGN KEY (`description_id`) REFERENCES `media_description` (`id`) ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `media_category`
+--
+ALTER TABLE `media_category`
+  ADD CONSTRAINT `media_category_categoryId` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `media_category_mediaId` FOREIGN KEY (`description_id`) REFERENCES `media_description` (`id`) ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `timestamp`
+--
+ALTER TABLE `timestamp`
+  ADD CONSTRAINT `FK_timestamp_media` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`),
+  ADD CONSTRAINT `FK_timestamp_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `FK_user_design` FOREIGN KEY (`design_id`) REFERENCES `design` (`id`),
+  ADD CONSTRAINT `question_id` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`);
+
+--
+-- Constraints for table `watch_list`
+--
+ALTER TABLE `watch_list`
+  ADD CONSTRAINT `FK_Verlauf_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_watch_history_media_description` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`);
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
