d57873a2d64222eae1be421de263011c33592d04
ortnm12 <ortnm12@hotmail.com>
2017-12-18 08:18:07 +0100

    reset password by benjamin

--- src/AppBundle/Controller/User/Security/SecurityController.php
+++ src/AppBundle/Controller/User/Security/SecurityController.php
@@ -21,6 +21,16 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SecurityController extends Controller
 {
+    public function setErrorCode($error, $error_count, $error_title, $error_message)
+    {
+
+        $error[$error_count]['error_title']= $error_title;
+        $error[$error_count]['error_message']= $error_message;
+
+        return $error;
+
+    }
+
     /**
      * @Route("/user/security/login", name="login")
      */
@@ -51,47 +61,125 @@ class SecurityController extends Controller
      */
     public function resetAction(Request $request,AuthenticationUtils $authUtils, UserPasswordEncoderInterface $passwordEncoder, Connection $connection, EncoderFactory $encoderFactory)
     {
-        if(isset($_POST['_username'])&& isset($_POST['form']['question']) && isset($_POST['answer']) && isset($_POST['new-password'])&& isset($_POST['new-password-repeat']))
-        {
-            $result = $connection->fetchAll("select answer, username, question_id from user where username ='$_POST[_username]'");
-            if ($_POST['form']['question'] == $result[0]['question_id'])
-            {
-                $user = new User();
-                $answer = $passwordEncoder->encodePassword($user, $_POST['answer']);
-                $encoder = $encoderFactory->getEncoder($user);
-                $res = $encoder->isPasswordValid($result[0]['answer'],$_POST['answer'],$user->getSalt());
 
-                if($res)
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+         $bool_username = true;
+         $bool_question = true;
+         $bool_answer = true;
+         $bool_new_password = true;
+         $bool_new_password_repeat = true;
+         $result = null;
+
+         $error_count = 0;
+
+         $error = array();
+
+         if(isset($_POST['_username']) && isset($_POST['answer']) && isset($_POST['new-password'])&& isset($_POST['new-password-repeat']))
+         {  
+           if (isset($_POST['_username']))
+           {
+            $result = $connection->fetchAll("select username, question_id, answer from user where username = '$_POST[_username]'");
+
+            if (empty($result))
+            {
+                $error = SecurityController::setErrorCode($error, $error_count,'Benutzer Fehler', 'Benutzer existiert nicht');
+                $bool_username = false;
+                $error_count++;
+            }
+        }
+        else
+        {
+            $error = SecurityController::setErrorCode($error, $error_count,'Benutzer Fehler', 'Es wurde kein Benutzer eingetragen');            
+            $bool_username = false;
+            $error_count++;
+        } 
+
+        if ($_POST['form']['question'] == "") 
+        {
+            $error = SecurityController::setErrorCode($error, $error_count,'Sicherheitsfrage Fehler', 'Es wurde keine Sicherheitsfrage ausgewählt!');
+            $bool_question = false;
+            $error_count++;
+        }
+
+        if (!isset($_POST['answer']))
+        {
+            $error = SecurityController::setErrorCode($error, $error_count,'Antwort Fehler', 'Geben Sie Ihre Antwort ein');
+            $bool_answer = false;
+            $error_count++;
+        }
+
+        if (!isset($_POST['new-password']))
+        {
+            $error = SecurityController::setErrorCode($error, $error_count,'Passwwort Fehler', 'Es wurde kein neues Passwort eingegeben');
+            $bool_new_password = false;
+            $error_count++;
+        }
+
+        if (!isset($_POST['new-password-repeat']))
+        {
+            $error = SecurityController::setErrorCode($error, $error_count,'Passwwort Fehler', 'Das wiederholte neue Passwort wurde nicht eingegeben');
+            $bool_new_password_repeat = false;
+            $error_count++;
+        }
+        if ($bool_username && $_POST['form']['question'] == $result[0]['question_id'])
+        {       
+            $user = new User();
+            $encoder = $encoderFactory->getEncoder($user);
+            $res = $encoder->isPasswordValid($result[0]['answer'],$_POST['answer'],$user->getSalt());
+
+            if($res)
+            {
+                if ($_POST['new-password'] == $_POST['new-password-repeat'])
                 {
-                    if ($_POST['new-password'] == $_POST['new-password-repeat'])
-                    {
-                        $password = $passwordEncoder->encodePassword($user, $_POST['new-password']);
-                        $connection->executeQuery("UPDATE user SET password = '$password' WHERE username = '$_POST[_username]'");
-                    }
+                    $password = $passwordEncoder->encodePassword($user, $_POST['new-password']);
+                    $connection->executeQuery("UPDATE user SET password = '$password' WHERE username = '$_POST[_username]'");
                 }
+                else
+                {
+                    $error = SecurityController::setErrorCode($error, $error_count,'Passwort Fehler', 'Die Passwörter stimmen nicht überein!');
+                    $bool_new_password = false;
+                    $error_count++;
+                }
+            }
+            else
+            {
+                $error = SecurityController::setErrorCode($error, $error_count, 'Antwort Fehler', 'Die Antworten stimmen nicht überein!');
+                $bool_answer = false;
+                $error_count++;
             }
         }
 
-        $result = $connection->fetchALl("select * from question");
-        $questionsTransformed = array();
-        foreach($result as $results){
-            $questionsTransformed[$results["text"]] = $results["id"];
-        }
 
-
-        $form = $this->createFormBuilder(null)
-            ->add("question", ChoiceType::class, array(
-                'choices' => $questionsTransformed
-            ))
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        return $this->render(
-            'security/reset_password.html.twig',
-            array('form' => $form->createView())
-        );
     }
+
+    if (!$bool_username || !$bool_answer || !$bool_question || !$bool_new_password || !$bool_new_password_repeat)
+    {
+        return new JsonResponse(json_encode($error),'418');
+    }
+}
+
+$result = $connection->fetchALl("select * from question");
+$questionsTransformed = array();
+foreach($result as $results){
+    $questionsTransformed[$results["text"]] = $results["id"];
+}
+
+
+
+
+$form = $this->createFormBuilder(null)
+->add("question", ChoiceType::class, array(
+    'choices' => $questionsTransformed
+))
+->getForm();
+
+$form->handleRequest($request);
+
+return $this->render(
+    'security/reset_password.html.twig',
+    array('form' => $form->createView())
+);
+}
 
     /**
      * @Route("/user/security/register", name="user_registration")
@@ -123,22 +211,22 @@ class SecurityController extends Controller
 
 
         $form = $this->createFormBuilder($result)
-            ->add("username", TextType::class)
-            ->add('password', RepeatedType::class, array(
-                'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password'),
-            ))
-            ->add("birthday", DateType::class, array('widget' => 'single_text',
-                'input' => 'string'
-                ))
-            ->add("question", ChoiceType::class, array(
+        ->add("username", TextType::class)
+        ->add('password', RepeatedType::class, array(
+            'type' => PasswordType::class,
+            'first_options'  => array('label' => 'Password'),
+            'second_options' => array('label' => 'Repeat Password'),
+        ))
+        ->add("birthday", DateType::class, array('widget' => 'single_text',
+            'input' => 'string'
+        ))
+        ->add("question", ChoiceType::class, array(
                 //Danke Maxi
-                'choices' => $questionsTransformed
-            ))
-            ->add("answer", TextType::class)
-            ->add("Registrieren", SubmitType::class)
-            ->getForm();
+            'choices' => $questionsTransformed
+        ))
+        ->add("answer", TextType::class)
+        ->add("Registrieren", SubmitType::class)
+        ->getForm();
 
         $form->handleRequest($request);
 
@@ -170,5 +258,9 @@ class SecurityController extends Controller
             array('form' => $form->createView())
         );
 
+
     }
+
+    
+
 }
